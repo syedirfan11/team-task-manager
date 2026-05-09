538f0e1eec38dd6933dd95f5a30dbfb8643edb17
Syed Irfan <syedirfan77860@gmail.com>
2026-05-09 11:46:53 +0530

    Connected frontend to Railway backend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -16,12 +16,12 @@ function Login() {
         try {
 
             const response = await axios.post(
-                "http://localhost:5000/api/auth/login",
-                {
-                    email,
-                    password
-                }
-            );
+    "https://team-task-manager-production-d543.up.railway.app/api/auth/login",
+    {
+        email,
+        password
+    }
+);
 
             alert(response.data.message);
 
