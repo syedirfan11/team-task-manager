40512a16a7e5db370209afaa9c0b25e41975b19a
Syed Irfan <syedirfan77860@gmail.com>
2026-05-09 11:56:03 +0530

    Fixed backend CORS

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,16 @@
-const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
 require("dotenv").config();
 
 const authRoutes = require("./routes/authRoutes");
-
 const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
 
 app.use(express.json());
+
 app.use(cors());
 
 mongoose.set("strictQuery", false);
@@ -33,7 +34,7 @@ app.get("/", (req, res) => {
     res.send("API Running");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
